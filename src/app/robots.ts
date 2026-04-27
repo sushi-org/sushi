54afc8b0463fa97e34dc154f5cc8c72577b6d4a8
lamt3 <lamtomoki@gmail.com>
2026-04-27 21:19:22 +0800

    Create robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,21 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "facebookexternalhit",
+        allow: "/",
+      },
+      {
+        userAgent: "Facebot",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/admin/", "/dashboard/", "/onboarding/"],
+      },
+    ],
+  };
+}
